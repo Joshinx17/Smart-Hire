342d7c3dc89dacdba4f9d888d9376d5fc1acae6e
Joshin <joshinkoshys@gmail.com>
2026-02-27 00:13:56 +0530

    deleted redundant files

--- smarthire/app/layout.tsx
+++ smarthire/app/layout.tsx
@@ -1,5 +1,3 @@
-import "./globals.css";
-
 export const metadata = {
   title: "SmartHire",
   description: "AI Powered Job Portal",
@@ -12,20 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <nav className="w-full px-6 py-4 bg-white shadow flex justify-between">
-          <h1 className="font-bold text-xl text-blue-600">
-            SmartHire
-          </h1>
-
-          <div className="space-x-4">
-            <button className="text-gray-700">Login</button>
-            <button className="text-gray-700">Register</button>
-          </div>
-        </nav>
-
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
